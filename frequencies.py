db6a15196578ee8971094f8a48c4b90aa034788e
Isaac Leegit config --global user.name Isaac <shun.s.lee@kcl.ac.uk>
2022-12-03 17:44:52 +0000

    Completed engagement task

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if isinstance(item, str):    
+            if item in frequencies:
+                frequencies[item] +=1
+            else:
+                frequencies[item] = 1
+        elif (isinstance(item, int)):
+            x = str(item)
+            if x in frequencies:
+                frequencies[x] +=1
+            else:
+                frequencies[x] = 1
     return frequencies
